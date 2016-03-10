c5cda2e37c78d3c8208dfc080e15c30d673f1af3
moodboom [ubuntu-melange] <m@thedigitalmachine.com>
2016-03-10 00:15:59 -0500

    scripts in flux

--- version-control.js
+++ version-control.js
@@ -1,5 +1,58 @@
 #!/usr/bin/env node
 
+
+// =========== git_sync: commits, then pulls, then pushes to the default remote repo ============
+var git_sync = function(folder,comment)
+{
+    // MDM NOTE: NEVER prompt/pause/sleep within "sub"-scripts run by parent scripts.
+
+    process.chdir(folder);
+
+    // Run these in order.  That means nest the calls, w00t!
+    var cmd1 = 'git commit -a' + comment;
+    var cmd2 = 'git pull';
+    var cmd3 = 'git push';
+
+    exec(cmd1, function(error, stdout, stderr) {
+
+        // Sometimes stupid emacs fails with "emacs: standard input is not a tty".
+        // I can't BELIEVE I used that as my primary editor for years!!  Think globally, EDIT LOCALLY.
+        if (stderr) {
+
+            console.log(stdout);
+            console.log(stderr);
+
+        } else {
+
+            exec(cmd2, function(error, stdout, stderr) {
+                if (error) {
+                    console.log(stdout);
+                    console.log(stderr);
+                    console.log('==================== GIT PULL FAILED ====================\n');
+                } else {
+                    exec(cmd3, function(error, stdout, stderr) {
+                        if (error) {
+                            console.log(stdout);
+                            console.log(stderr);
+                            console.log('==================== GIT PUSH FAILED ====================\n');
+                        } else
+                        {
+                            // MDM Eventually remove this chatter.
+                            // console.log(stdout);
+                            // console.log(stderr);
+                            // console.log('==================== SYNC COMPLETE ====================\n');
+                            // console.log("<=> * [" + process.cwd() + "] " + cmd1 + " && " + cmd2 + " && " + cmd3);
+
+                            console.log("<=> " + process.cwd());
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
+
+
 // =========== svn_last_changed_rev: gets the SVN "last changed rev" for the current folder, as a string ============
 var svn_last_changed_rev = function () {
 
@@ -31,5 +84,7 @@ var svn_rev = function () {
     return array_result[1];
 }
 
+
+module.exports.git_sync = git_sync;
 module.exports.svn_last_changed_rev = svn_last_changed_rev;
 module.exports.svn_rev = svn_rev;

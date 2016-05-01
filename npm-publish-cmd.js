aaac111ddd1b2a0437a09486c413d20032b5b73e
moodboom [ubuntu-melange] <m@thedigitalmachine.com>
2016-05-01 15:23:42 -0400

    fixes to npm-publish

--- npm-publish-cmd.js
+++ npm-publish-cmd.js
@@ -2,7 +2,7 @@
 
 var fs = require('fs');
 var ru = require('./run-utils.js');
-var vc = require('./version-control.js';
+var vc = require('./version-control.js');
 
 var version = vc.git_version_clean();
 console.log('Stamping version ['+version+']...');
@@ -10,10 +10,10 @@ console.log('Stamping version ['+version+']...');
 try {
 
     var filename = 'package.json';
-    var origversion = fs.readFileSync(filename);
+    var origversion = fs.readFileSync(filename,'utf-8');
     //   "version": "1.3.0",  ==>    "version": "###git_version_clean###",
-    var newversion = origversion.replace(/\"version\".*$/, '\"version\": \"'+version+'\",');
-    fs.writeFileSync(filename, newversion);
+    var newversion = origversion.replace(/\"version\".*/, '\"version\": \"'+version+'\",');
+    fs.writeFileSync(filename, newversion,'utf-8');
     console.log(filename + " was updated...");
 
     ru.run_command_sync_to_console('npm publish');

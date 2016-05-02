990b70f9f12b976a5e2e757b85917a0862a2a88b
Michael Behrns-Miller [carambola] <moodboom@gmail.com>
2016-05-02 16:17:34 -0400

    testing auto-commit on tag again

--- rs-publish-cmd.js
+++ rs-publish-cmd.js
@@ -8,19 +8,14 @@ var version = vc.git_version_clean();
 console.log('Stamping version ['+version+']...');
 
 // Dogfood rad-scripts to publish rad-scripts!
-
 try {
 
-    var readme = ru.run_command_sync('rad');
-
-    var filename = 'README.md';
-    fs.writeFileSync(filename, readme,'utf-8');
-    console.log(filename + " was updated...");
+    var readme = ru.run_command_sync('rs-update-readme');
 
     // Note: this will push the README.md change for us.
     ru.run_command_sync_to_console('npm-publish');
 }
 catch (err) {
-    console.log("Version could not be updated: "+err);
+    console.log("Error publishing: "+err);
     process.exit(1);
 }

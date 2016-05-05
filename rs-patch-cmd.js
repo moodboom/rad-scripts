0bf35dd90b57386e8ea81492b79f5b39e4fd8bec
Michael Behrns-Miller [carambola] <moodboom@gmail.com>
2016-05-05 13:11:14 -0400

    adding missing rs dogfood files

---
+++ rs-patch-cmd.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+var vc = require('./version-control.js');
+var ru = require('./run-utils.js');
+
+var args = process.argv.slice(2);
+var comment = ru.combine_params(args);
+
+// top-level dev script to commit tag and publish
+vc.git_tag_patch(comment);
+ru.run_command_sync_to_console('rs-publish');

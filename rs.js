b8809990afc44f27b170c5097bb9ca027cc35aa2
moodboom [ubuntu-melange] <m@thedigitalmachine.com>
2016-05-14 10:03:06 -0400

    added missing rs.js file

---
+++ rs.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var vc = require('./version-control.js');
+var ru = require('./run-utils.js');
+
+var rs_publish = function(version,comment) {
+
+    vc.npm_update_version(version);
+
+    // Reinstall, so we get any recently-made changes to usage.
+    ru.run_command_sync_to_console('npm install -g');
+
+    // Directly update README.md with usage, whoop
+    var readme = ru.run_command_sync('rad');
+    var filename = 'README.md';
+    fs.writeFileSync(filename, readme,'utf-8');
+    console.log(filename + " was updated...");
+
+    // commit + pull + tag + push
+    vc.git_sync('.',comment,version);
+
+    // npm publish
+    ru.run_command_sync_to_console('npm publish');
+}
+
+module.exports.rs_publish = rs_publish;

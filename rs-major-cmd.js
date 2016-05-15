d791b54fd40d895f1cc5d32487da386a73a7d24b
moodboom [ubuntu-melange] <m@thedigitalmachine.com>
2016-05-15 09:30:42 -0400

    rs-major changes never got pushed

--- rs-major-cmd.js
+++ rs-major-cmd.js
@@ -2,24 +2,10 @@
 
 var vc = require('./version-control.js');
 var ru = require('./run-utils.js');
+var rs = require('./rs.js');
 
 var args = process.argv.slice(2);
 var comment = ru.combine_params(args);
-
 var version = vc.git_next_major();
-vc.npm_update_version(version);
 
-// Reinstall, so we get any recently-made changes to usage.
-ru.run_command_sync_to_console('npm install -g');
-
-// Directly update README.md with usage, whoop
-var readme = ru.run_command_sync('rad');
-var filename = 'README.md';
-fs.writeFileSync(filename, readme,'utf-8');
-console.log(filename + " was updated...");
-
-// commit + pull + tag + push
-vc.git_sync('.',comment,version);
-
-// npm publish
-ru.run_command_sync_to_console('npm publish');
+rs.rs_publish(version,comment);

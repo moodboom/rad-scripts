e2a75eb511653987c162ebd809e96136de6f962a
moodboom [ubuntu-melange] <m@thedigitalmachine.com>
2016-05-15 16:19:51 -0400

    do not bump to next version if there were no code changes

--- rs.js
+++ rs.js
@@ -6,8 +6,10 @@ var ru = require('./run-utils.js');
 
 var rs_publish = function(comment,version) {
 
-    if (vc.git_version_valid(version))
+    // If there have been code changes, stamp the "next" version into the app.
+    if (vc.git_version_valid(version) && ru.git_changes().length > 0) {
         vc.npm_update_version(version);
+    }
 
     // Reinstall, so we get any recently-made changes to usage.
     ru.run_command_sync_to_console('npm install -g');
@@ -19,6 +21,7 @@ var rs_publish = function(comment,version) {
     console.log(filename + " was updated...");
 
     // commit + pull + tag + push
+    // NOTE that at this point, we will not have changed anything if there were no code changes, so we won't get a commit.
     if (vc.git_sync('.',comment,version)) {
 
         // There were changes, so let's publish now.

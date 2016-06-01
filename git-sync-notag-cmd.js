e5a6d6d25bc51d15d2e62608289474ed34b11638
Michael Behrns-Miller [wallee-ubuntu] <m@thedigitalmachine.com>
2016-06-01 16:36:00 -0400

    git-sync will now automatically create tag 0.0.0 if no semver tag exists

---
+++ git-sync-notag-cmd.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+var exec = require('child_process').exec;
+var vc = require('./version-control.js');
+
+// This script is designed to be run from within a git repo.
+// You can pass any number of params to the command; they will all be concatenated into a single commit message.
+
+var tag_params = vc.parse_tag_parameters(process.argv);
+
+// We don't provide the version here, as this command is specifically used to avoid tagging.
+// You should consider using git-sync instead!
+vc.git_sync(process.cwd(),tag_params.comment);
